857f2e7b48580f0476789121b087fcfaf7298144
Crypticwave <abhinavdatta38@gmail.com>
2021-04-05 18:01:19 +0530

    Fixing prism nord and gitignore

--- components/Editor.js
+++ components/Editor.js
@@ -8,7 +8,7 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism-dark.css"; //Example style, you can use another
+import "prism-themes/themes/prism-nord.css"; //Example style, you can use another
 
 
 export default function CodeEditor(){
